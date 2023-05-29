73c016ca66ac3161ba86efd756cdb0235defbad3
Gaurav Joshi <gaurav@Gauravs-MacBook-Air.local>
2023-05-29 20:52:46 +0530

    first commit

---
+++ src/About.js
@@ -0,0 +1,18 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+function About(){
+    return(
+        <div>
+            <Container fluid className='about' id='about'>
+
+            </Container>
+        </div>
+    )
+}
+
+export default About;
