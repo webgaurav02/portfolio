b494faf7c09e7facc663e683584d03e72b4c98eb
Gaurav Joshi <gaurav@Gauravs-MacBook-Air.local>
2023-06-04 16:15:24 +0530

    Made all sections responsive

--- src/About.js
+++ src/About.js
@@ -2,8 +2,10 @@ import React, { useEffect, useRef } from "react";
 import "./About.css";
 import Container from "react-bootstrap/Container";
 import Footer from "./Footer";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 
-const disp = "</>"
+const disp = "</>";
 
 function About() {
   const aboutRef = useRef(null);
@@ -41,25 +43,34 @@ function About() {
         <h1 className="fade-in">I'm Gaurav.</h1>
         <h2 className="fade-in">
           Welcome to my portfolio website! I'm a{" "}
-          <span className="a-big fade-in">Student</span> at the National Institute of
-          Technology Meghalaya and a <span className="a-big fade-in">Web Developer.</span>
+          <span className="a-big fade-in">Student</span> at the National
+          Institute of Technology Meghalaya and a{" "}
+          <span className="a-big fade-in">Web Developer.</span>
         </h2>
-        <p className="fade-in">
-          I rock at HTML, CSS, and JavaScript, and I'm not too shabby with
-          React.js either.
-        </p>
-        <p className="fade-in">
-          Oh, and did I mention I dabble in{" "}
-          <span className="a-big fade-in">graphic design</span> too?
-        </p>
-        <p className="fade-in">
-          Take a peek at my portfolio and see the magic I can create with a mix
-          of creativity and coding skills. Let's make something awesome
-          together!
-        </p>
-        <h1 className="code-logo fade-in">{disp}</h1>
+        <div>
+          <Row>
+            <Col lg={6}>
+              <p className="fade-in">
+                I rock at HTML, CSS, and JavaScript, and I'm not too shabby with
+                React.js either.
+              </p>
+              <p className="fade-in">
+                Oh, and did I mention I dabble in{" "}
+                <span className="a-big fade-in">graphic design</span> too?
+              </p>
+              <p className="fade-in">
+                Take a peek at my portfolio and see the magic I can create with
+                a mix of creativity and coding skills. Let's make something
+                awesome together!
+              </p>
+            </Col>
+            <Col lg={6} className="img-colm">
+              <h1 className="code-logo fade-in">{disp}</h1>
+            </Col>
+          </Row>
+        </div>
       </Container>
-      <Footer section=""/>
+      <Footer section="" />
     </>
   );
 }
