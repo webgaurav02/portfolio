76ea85993ae3e82667635a4ca49e883b6ae614f4
Gaurav Joshi <gaurav@Gauravs-MacBook-Air.local>
2024-01-08 21:35:31 +0530

    Add Work section w/o responsiveness

---
+++ src/Components/ProjectItem.js
@@ -0,0 +1,28 @@
+import { Row, Col } from "react-bootstrap";
+import "../styles/ProjectItem.css"
+
+const ProjectItem = (props) => {
+    return (
+        <>
+            <Col md={6}>
+                <Row className="client-card">
+                    <Col xs={9}>
+                        <h4> {props.title} </h4>
+                        <p> {props.description} </p>
+                        <div>
+                            <div className="live-button">
+                                <a href={props.link} target="_blank" rel="noreferrer" > View live </a>
+                            </div>
+                            {props.github && <div className="github-button"> <a href={props.link} target="_blank" rel="noreferrer"> Github </a> </div>}
+                        </div>
+                    </Col>
+                    <Col xs={3} className="client-img-container">
+                        <img className="client-img" src={props.image} alt="Client Logo" />
+                    </Col>
+                </Row>
+            </Col>
+        </>
+    )
+}
+
+export default ProjectItem;
--- src/Components/Work.js
+++ src/Components/Work.js
@@ -1,7 +1,10 @@
 import React, { useEffect } from "react";
 import "../styles/Work.css";
 import Container from "react-bootstrap/Container";
+import { Row } from "react-bootstrap";
 import Footer from "./Footer"
+import WorkData from "../WorkData.json"
+import ProjectItem from "./ProjectItem";
 
 function Work() {
   useEffect(() => {
@@ -10,13 +13,40 @@ function Work() {
       element.classList.add("fade-in-animation");
     });
   }, []);
-  
+
   return (
     <>
       <Container fluid className="work">
-        <h1 className="fade-in">Coming Soon...</h1>
+        <h3 className="fade-in">Clients</h3>
+        <Row className="fade-in">
+          {WorkData.clients.map(ele => {
+            return (
+                <ProjectItem
+                  title={ele.title}
+                  description={ele.description}
+                  image={ele.image}
+                  link={ele.link}
+                  github={ele.github}
+                />
+            );
+          })}
+        </Row>
+        <h3 className="fade-in">Projects</h3>
+        <Row className="fade-in">
+          {WorkData.projects.map(ele => {
+            return (
+                <ProjectItem
+                  title={ele.title}
+                  description={ele.description}
+                  image={ele.image}
+                  link={ele.link}
+                  github={ele.github}
+                />
+            );
+          })}
+        </Row>
       </Container>
-      <Footer section=""/>
+      <Footer section="" />
     </>
   );
 }
